4a517589312d20ef723ece76b427f9f53ef7db21
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-03-04 10:54:39 -0500

    tweat provided neato and sim files

--- sim.m
+++ sim.m
@@ -1,8 +1,10 @@
 clear all; close all; clc;
 
-[sensors,vels]=neatoSim(); % create the simulator
+[sensors,vels]=customSim(); % create the simulator
 pause(5); % give the simulator a few seconds to load
 
 [new_angle, new_loc] = move_point(0, [0,0], [0,4], sensors, vels);
 [new_angle, new_loc] = move_point(new_angle, new_loc, [4,4], sensors, vels);
 [new_angle, new_loc] = move_point(new_angle, new_loc, [4,0], sensors, vels);
+
+neato()
