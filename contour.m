9860e060931f0ae8bc40bc015ff3d70511a6b411
Sparsh Gupta <sparshg@gmail.com>
2023-03-06 20:28:40 -0500

    Updated Contour Plot code

--- contour.m
+++ contour.m
@@ -2,14 +2,17 @@ syms x y
 
 %grab points
 R = backtracking_descent();
+
 %split into x and y points
 Rx=R(:,1);
 Ry=R(:,2);
 
 %plot function points as meshs
 [xplot, yplot] = meshgrid(-2:.2:2, -2:.2:2);
+
 %function as mesh
 fplot = 16 .* (exp(((-xplot .^ 2)./2) - ((yplot .^ 2)./2) - ((xplot .* yplot)./2)) ) + 4 .* (exp(-(xplot + 1.5).^2 - (yplot + 2.5).^2)); % plots x y and f as a 3d surface.   
+
 f = 16 * exp(-(x.^2)/2 - (y.^2)/2 - (x*y)/2 ) + 4 * exp(-(x + 1.5).^2 - (y + 2.5).^2);
 
 %calculate gradient vector
@@ -36,4 +39,5 @@ colorbar
 % x y and title labels
 xlabel("Distance (m)")
 ylabel("Distance (m)")
-title("Contour plot with theoretical path")
+title("Contour plot with Theoretical Path")
+legend("Gradient", "Position vectors")
